6c1ba9b63947d7a52f43e926e9713244b8115c3a
SamuraiCoding <103792284+SamuraiCoding-git@users.noreply.github.com>
2025-04-07 06:55:42 +0300

    Initial Commit

--- tgbot/handlers/user.py
+++ tgbot/handlers/user.py
@@ -65,7 +65,7 @@ async def user_start(message: Message, config: Config):
         return
     text = (
         f"При использовании бота вы соглашаетесь с "
-        f"{hlink('офертой', 'https://e1daea51-9c17-4933-adcb-86779a620982.selstorage.ru/250216%20%D0%94%D0%BE%D0%B3%D0%BE%D0%B2%D0%BE%D1%80-%D0%BE%D1%84%D0%B5%D1%80%D1%82%D0%B0.docx')} "
+        f"{hlink('офертой', 'https://e1daea51-9c17-4933-adcb-86779a620982.selstorage.ru/250405_%D0%9E%D1%84%D0%B5%D1%80%D1%82%D0%B0_%D0%B4%D0%BE%D1%81%D1%82%D1%83%D0%BF_%D0%BA_%D0%A2%D0%93_%D0%BA%D0%B0%D0%BD%D0%B0%D0%BB%D1%83_%D0%B8%D1%81%D0%BF%D1%80.docx')} "
         f"и {hlink('политикой конфиденциальности', 'https://e1daea51-9c17-4933-adcb-86779a620982.selstorage.ru/%D0%9F%D0%BE%D0%BB%D0%B8%D1%82%D0%B8%D0%BA%D0%B0_%D0%BA%D0%BE%D0%BD%D1%84%D0%B8%D0%B4%D0%B5%D0%BD%D1%86%D0%B8%D0%B0%D0%BB%D1%8C%D0%BD%D0%BE%D1%81%D1%82%D0%B8_%D0%98%D0%9F_%D0%9F%D0%B0%D0%BD%D1%82%D0%B5%D0%BB%D0%B0.docx')}."
     )
     await message.answer(text, reply_markup=offer_keyboard())
