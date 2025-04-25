1a3a2dd0164667536d9e1cfdf07f7a7dfb32a13f
SamuraiCoding <103792284+SamuraiCoding-git@users.noreply.github.com>
2025-04-25 21:25:23 +0300

    Initial Commit

--- tgbot/handlers/user.py
+++ tgbot/handlers/user.py
@@ -174,8 +174,7 @@ async def user_deeplink(message: Message, command: CommandObject, state: FSMCont
     else:
         deeplink = await repo.deeplink.get_deeplink_by_id(int(command.args))
         scenario_handler = ScenarioHandler(message, state, config)
-        print(deeplink.scenario)
-        await scenario_handler.handle_scenario(deeplink.scenario)
+        await scenario_handler.handle_scenario(json.loads(deeplink.scenario))
 
 
 @user_router.message(CommandStart())
@@ -209,7 +208,7 @@ async def accept_offer(call: CallbackQuery, config: Config, state: FSMContext):
     if data.get("deeplink"):
         deeplink = await repo.deeplink.get_deeplink_by_id(int(data.get("deeplink")))
         scenario_handler = ScenarioHandler(call.message, state, config)
-        await scenario_handler.handle_scenario(deeplink.scenario)
+        await scenario_handler.handle_scenario(json.loads(deeplink.scenario))
     else:
         await state.clear()
         text = config.messages.course_intro
